d93f78f4829126260dedc72b6cc204bfb74e1f5d
Mahsa.azizi <mahsa.azizi@eligasht.com>
2018-03-11 16:01:40 +0330

    passenger

--- app/src/main/java/com/eligasht/reservation/views/ui/PassengerHotelActivity.java
+++ app/src/main/java/com/eligasht/reservation/views/ui/PassengerHotelActivity.java
@@ -1648,7 +1648,7 @@ public class PassengerHotelActivity extends BaseActivity implements Header.onSea
                             flagMosafer=flagMosafer+"T";
                         }else{
                             flagMosafer=flagMosafer+"F";
-                            errorMessage=errorMessage+"\n"+getString(R.string.Please_choose_a_gender);
+                            errorMessage=errorMessage+"\n"+"* "+getString(R.string.Please_choose_a_gender);
                         }
                         //////////////////////////End Validate
                         if (flagMosafer.contains("F")) {
