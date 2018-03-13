9f53aeddd8b0e96fc4e4636f6d9e1d4fcf784429
Reza.nejati <eli123456>
2018-03-13 15:12:37 +0330

    Merge branch 'azizi3' of http://192.168.115.210:8080/tfs/Eligasht%20Android%20App%20Ver.1/_git/EligashtAndroidApp
    # Conflicts:
    #	app/src/main/res/values-sw600dp/dimens.xml
    #	app/src/main/res/values-sw720dp/dimens.xml

--- app/src/main/java/com/eligasht/reservation/views/adapters/ExpandableListAdapter.java
+++ app/src/main/java/com/eligasht/reservation/views/adapters/ExpandableListAdapter.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
 import android.text.Html;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -22,6 +24,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
 import com.eligasht.reservation.tools.ExpandableListViewE;
@@ -493,7 +499,18 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
                 .with(_context)
                 .load(imageUri)
                 .centerCrop()
-                .error(R.drawable.not_found)
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        avi.setVisibility(View.GONE);
+                        return false;
+                    }
+                })
                 .into(lblProductrow);
 
 
