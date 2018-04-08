bf145bac9a7863cc53730619f47ffb121e99c1b6
Reza.nejati <eli123456>
2018-04-08 18:06:14 +0430

    Merge branch 'azizi3' of http://192.168.115.210:8080/tfs/Eligasht%20Android%20App%20Ver.1/_git/EligashtAndroidApp
    # Conflicts:
    #	app/src/main/java/com/eligasht/reservation/views/ui/PassengerHotelFlightActivity.java

--- app/src/main/java/com/eligasht/reservation/views/activities/hotel/activity/DetailHotelActivity.java
+++ app/src/main/java/com/eligasht/reservation/views/activities/hotel/activity/DetailHotelActivity.java
@@ -1,9 +1,7 @@
 package com.eligasht.reservation.views.activities.hotel.activity;
-
 /**
  * Reza Nejati <reza.n.j.t.i@gmail.com>
  */
-
 import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.graphics.Color;
@@ -87,7 +85,6 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.gson.Gson;
-
 import com.wang.avi.AVLoadingIndicatorView;
 
 import org.greenrobot.eventbus.EventBus;
@@ -103,15 +100,11 @@ import java.util.Set;
 
 import at.grabner.circleprogress.CircleProgressView;
 import mehdi.sakout.fancybuttons.FancyButton;
-
-
-public class DetailHotelActivity extends BaseActivity implements View.OnClickListener{
-
+public class DetailHotelActivity extends BaseActivity implements View.OnClickListener {
     private TextView tvTitle;
     private ArrayList<RoomsModel> roomsModels = new ArrayList<>();
-    private ArrayList<CommentModel> commentModels = new ArrayList<>();
-    private RelativeLayout rlLoading, rlLoading2;
-    CoordinatorLayout rlRoot;
+    private RelativeLayout rlLoading2;
+    private CoordinatorLayout rlRoot;
     private GetRoomsList getRoomsList;
     private Window window;
     private HotelDetailViewPager hotelDetailViewPager;
@@ -121,15 +114,13 @@ public class DetailHotelActivity extends BaseActivity implements View.OnClickLis
     private GetHotelDetail getHotelDetail;
     private TextView tvHotelName, tvCityName, tvAdress, tvAlertError;
     private ImageView ivImage;
-    private TextView tvSortComment, tvDateDetail;
-    boolean isNew = false;
+    private TextView tvDateDetail;
     private RelativeLayout elNotFound;
     private CardView cvHotel;
     private FrameLayout flViewPager;
     private TabLayout tab_layout;
-    CommentModelBus commentModelBus;
-    SwipeBackActivityHelper helper = new SwipeBackActivityHelper();
-
+    private CommentModelBus commentModelBus;
+    private SwipeBackActivityHelper helper = new SwipeBackActivityHelper();
     private TabLayout.OnTabSelectedListener onTabSelectedListener = new TabLayout.OnTabSelectedListener() {
         @Override
         public void onTabSelected(TabLayout.Tab tab) {
@@ -138,20 +129,17 @@ public class DetailHotelActivity extends BaseActivity implements View.OnClickLis
                     hotelDetailViewPager.getCommentHotelFragment().setDataComment(commentModelBus);
                     break;
                 case 1:
-                    hotelDetailViewPager.getMapHotelFragment().setMarker(location,commentModelBus);
+                    hotelDetailViewPager.getMapHotelFragment().setMarker(location, commentModelBus);
                     break;
             }
-
         }
 
         @Override
         public void onTabUnselected(TabLayout.Tab tab) {
-
         }
 
         @Override
         public void onTabReselected(TabLayout.Tab tab) {
-
         }
     };
 
@@ -168,18 +156,14 @@ public class DetailHotelActivity extends BaseActivity implements View.OnClickLis
         InitUi.Toolbar(this, false, R.color.toolbar_color, getString(R.string.DetailHotel));
         window = getWindow();
         initView();
-
-
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
     }
 
     public void initView() {
-        rlLoading = findViewById(R.id.rlLoading);
         flViewPager = findViewById(R.id.flViewPager);
         cvHotel = findViewById(R.id.cvHotel);
         rlRoot = findViewById(R.id.rlRoot);
@@ -188,34 +172,28 @@ public class DetailHotelActivity extends BaseActivity implements View.OnClickLis
         tvHotelName = findViewById(R.id.tvHotelName);
         tvCityName = findViewById(R.id.tvCityName);
         tvDateDetail = findViewById(R.id.tvDateDetail);
-        tvSortComment = findViewById(R.id.tvSortComment);
         elNotFound = findViewById(R.id.elNotFound);
         tvAlertError = findViewById(R.id.tvAlertError);
         rlLoading2 = findViewById(R.id.rlLoading2);
         tab_layout = findViewById(R.id.tab_layout);
         view_pager = findViewById(R.id.view_pager);
         toolbar = findViewById(R.id.toolbar);
-        initToolbar(toolbar);
         tvTitle = findViewById(R.id.tvTitle);
-        rlLoading2.setOnClickListener(this);
-        Utility.setAnimLoading(this);
         tvDateDetail.setText(getIntent().getExtras().getString("DateTime"));
-        hotelDetailViewPager = new HotelDetailViewPager(this, getSupportFragmentManager(),false);
+        hotelDetailViewPager = new HotelDetailViewPager(this, getSupportFragmentManager(), false);
         view_pager.setAdapter(hotelDetailViewPager);
         tab_layout.setupWithViewPager(view_pager);
         view_pager.setCurrentItem(3);
         try {
             if (getIntent().getExtras().getString("Type").equals("Pakage")) {
                 // lvRooms.setVisibility(View.GONE);
-                hotelDetailViewPager = new HotelDetailViewPager(this, getSupportFragmentManager(),true);
+                hotelDetailViewPager = new HotelDetailViewPager(this, getSupportFragmentManager(), true);
                 view_pager.setAdapter(hotelDetailViewPager);
                 tab_layout.setupWithViewPager(view_pager);
                 view_pager.setCurrentItem(2);
-
             }
         } catch (Exception e) {
         }
-
         view_pager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
@@ -224,15 +202,9 @@ public class DetailHotelActivity extends BaseActivity implements View.OnClickLis
             @Override
             public void onPageSelected(int position) {
                 if (position != 0) {
-                    /// if the current view page is not the first, make 'viewPager' receive touch event.
                     helper.disableSwipeBack();
-
-                    /// or enable edge mode
                 } else {
-                    /// the current page return to the first one, make 'swipe back' receive touch event.
                     helper.enableSwipeBack();
-
-                    /// or disable edge mode
                 }
             }
 
@@ -240,7 +212,6 @@ public class DetailHotelActivity extends BaseActivity implements View.OnClickLis
             public void onPageScrollStateChanged(int state) {
             }
         });
-
         tab_layout.setOnTabSelectedListener(onTabSelectedListener);
         ViewGroup vg = (ViewGroup) tab_layout.getChildAt(0);
         int tabsCount = vg.getChildCount();
@@ -250,43 +221,31 @@ public class DetailHotelActivity extends BaseActivity implements View.OnClickLis
             for (int i = 0; i < tabChildsCount; i++) {
                 View tabViewChild = vgTab.getChildAt(i);
                 if (tabViewChild instanceof TextView) {
-
                     ((TextView) tabViewChild).setTypeface(Typeface.createFromAsset(getAssets(), SingletonContext.getInstance().getContext().getResources().getString(R.string.iran_sans_normal_ttf)));
-
                 }
             }
         }
+        rlLoading2.setOnClickListener(this);
+        Utility.setAnimLoading(this);
+        initToolbar(toolbar);
         new GetRoomsAsync().execute();
-
     }
 
     @Override
     public void onClick(View v) {
-
         switch (v.getId()) {
-
             case R.id.btnOk:
                 finish();
                 break;
-
         }
-
     }
 
-
-
-
     private class GetRoomsAsync extends AsyncTask<String, Void, String> {
-
         protected void onPreExecute() {
             rlLoading2.setVisibility(View.VISIBLE);
-
-
             if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT) {
-
                 window.setStatusBarColor(ContextCompat.getColor(DetailHotelActivity.this, R.color.blue2));
             }
-
         }
 
         @Override
@@ -297,58 +256,36 @@ public class DetailHotelActivity extends BaseActivity implements View.OnClickLis
                         String.valueOf(getIntent().getExtras().getInt("HotelId")),
                         "", "", getIntent().getExtras().getString("ResultUniqID"),
                         getString(R.string.culture))));
-
             } catch (Exception e) {
-
             }
             return "Executed";
         }
 
         @Override
         protected void onPostExecute(String result) {
-
-
             try {
-
                 if (getRoomsList.getRoomsListResponse.GetRoomsListResult.errors != null) {
                     elNotFound.setVisibility(View.VISIBLE);
-
                     tvAlertError.setText(getRoomsList.getRoomsListResponse.GetRoomsListResult.errors.get(0).getMessage());
-
                 } else {
-
                     int i = 0;
                     for (RoomList roomList : getRoomsList.getRoomsListResponse.GetRoomsListResult.roomList) {
                         Log.e("testtest", roomList.Description);
-
                         roomsModels.add(new RoomsModel(roomList.Board, roomList.Title, roomList.Description, roomList.Price,
                                 roomList.OfferId, roomList.EHotelId, getRoomsList.getRoomsListResponse.GetRoomsListResult.SearchKey));
-
                     }
                     EventBus.getDefault().post(new RoomsModelBus(roomsModels));
                     new GetHoldDetailAsync().execute();
-
-
                 }
-
             } catch (Exception e) {
-
                 elNotFound.setVisibility(View.VISIBLE);
                 rlLoading2.setVisibility(View.GONE);
                 tvAlertError.setText(R.string.ErrorServer);
             }
-
-
         }
-
     }
-
-
     private class GetHoldDetailAsync extends AsyncTask<String, Void, String> {
-
         protected void onPreExecute() {
-
-
         }
 
         @Override
@@ -356,9 +293,7 @@ public class DetailHotelActivity extends BaseActivity implements View.OnClickLis
             try {
                 getHotelDetail = new GetHotelDetail(new GetHotelDetailRequest(new GetHotelDRequest(getString(R.string.culture), String.valueOf(getIntent().getExtras().getInt("HotelId")))));
                 Log.e("j2j2j2", new Gson().toJson(new GetHotelDetailRequest(new GetHotelDRequest(getString(R.string.culture), String.valueOf(getIntent().getExtras().getInt("HotelId"))))));
-
             } catch (Exception e) {
-
             }
             return "Executed";
         }
@@ -366,99 +301,65 @@ public class DetailHotelActivity extends BaseActivity implements View.OnClickLis
         @Override
         protected void onPostExecute(String result) {
             rlLoading2.setVisibility(View.GONE);
-
             if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT) {
-
                 window.setStatusBarColor(ContextCompat.getColor(DetailHotelActivity.this, R.color.colorPrimaryDark));
             }
-
-
             ArrayList<ImageModel> imageModels = new ArrayList<>();
-
             try {
                 cvHotel.setVisibility(View.VISIBLE);
-
-
                 YoYo.with(Techniques.FadeIn)
                         .duration(400)
                         .playOn(cvHotel);
-
-
                 tvHotelName.setText(getHotelDetail.getHotelDetailResult.GetHotelDetailResult.HotelDetail.HotelName);
                 tvHotelName.setVisibility(View.GONE);
                 tvTitle.setText(getHotelDetail.getHotelDetailResult.GetHotelDetailResult.HotelDetail.HotelName);
-
                 tvAdress.setText(getHotelDetail.getHotelDetailResult.GetHotelDetailResult.HotelDetail.Address);
                 Log.e("test", getHotelDetail.getHotelDetailResult.GetHotelDetailResult.HotelDetail.Address);
-
                 try {
                     location = new LatLng(Double.valueOf(getHotelDetail.getHotelDetailResult.GetHotelDetailResult.HotelDetail.Latitude),
                             Double.valueOf(getHotelDetail.getHotelDetailResult.GetHotelDetailResult.HotelDetail.Longitude));
-
                 } catch (Exception e) {
                 }
-
                 for (ImageHotel imageHotel : getHotelDetail.getHotelDetailResult.GetHotelDetailResult.HotelDetail.HotelImages) {
                     imageModels.add(new ImageModel(imageHotel.HotelImagesURL));
-
                 }
                 EventBus.getDefault().post(new HotelProprtiesBus(getHotelDetail.getHotelDetailResult.GetHotelDetailResult.HotelDetail.HotelProprties));
                 commentModelBus = new CommentModelBus(getHotelDetail.getHotelDetailResult.GetHotelDetailResult.HotelDetail.HotelName, String.valueOf(getIntent().getExtras().getInt("HotelId")));
-
                 EventBus.getDefault().post(new CommentModelBus(getHotelDetail.getHotelDetailResult.GetHotelDetailResult.HotelDetail.HotelName, String.valueOf(getIntent().getExtras().getInt("HotelId"))));
-
-
                 YoYo.with(Techniques.FadeIn)
                         .duration(400)
                         .playOn(flViewPager);
-
                 Collections.reverse(imageModels);
-
-
-                new ViewPagerAttention(DetailHotelActivity.this, imageModels, R.id.intro_view_pager,getHotelDetail.getHotelDetailResult.GetHotelDetailResult.HotelDetail.HotelName);
+                new ViewPagerAttention(DetailHotelActivity.this, imageModels, R.id.intro_view_pager, getHotelDetail.getHotelDetailResult.GetHotelDetailResult.HotelDetail.HotelName);
                 tvCityName.setText(getHotelDetail.getHotelDetailResult.GetHotelDetailResult.HotelDetail.CityName + " " + getString(R.string.comma) + " " + getHotelDetail.getHotelDetailResult.GetHotelDetailResult.HotelDetail.CountryName);
                 Log.e("star", getHotelDetail.getHotelDetailResult.GetHotelDetailResult.HotelDetail.StarRating + "");
-
                 switch (getHotelDetail.getHotelDetailResult.GetHotelDetailResult.HotelDetail.StarRating) {
-
                     case 1:
                         //todo change this
                         ivImage.setImageDrawable(ContextCompat.getDrawable(DetailHotelActivity.this, R.drawable._1star));
-
                         break;
                     case 2:
                         ivImage.setImageDrawable(ContextCompat.getDrawable(DetailHotelActivity.this, R.drawable._2star));
-
                         break;
                     case 3:
                         ivImage.setImageDrawable(ContextCompat.getDrawable(DetailHotelActivity.this, R.drawable._3star));
-
                         break;
                     case 4:
                         ivImage.setImageDrawable(ContextCompat.getDrawable(DetailHotelActivity.this, R.drawable._4star));
-
                         break;
-
                     case 5:
                         ivImage.setImageDrawable(ContextCompat.getDrawable(DetailHotelActivity.this, R.drawable._5star));
-
                         break;
                     case -1:
                         ivImage.setVisibility(View.GONE);
                         break;
                 }
-
-
             } catch (Exception e) {
                 Toast.makeText(DetailHotelActivity.this, R.string.ErrorServer, Toast.LENGTH_SHORT).show();
                 finish();
-
             }
-
         }
-
     }
-
 
     protected void initToolbar(Toolbar toolbar) {
         if (toolbar == null)
@@ -470,7 +371,6 @@ public class DetailHotelActivity extends BaseActivity implements View.OnClickLis
         actionBar.setDisplayHomeAsUpEnabled(false);
         actionBar.setDisplayShowHomeEnabled(false);
         actionBar.setDisplayUseLogoEnabled(false);
-
         View customView = getLayoutInflater().inflate(R.layout.toolbar, null);
         customView.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT));
         actionBar.setCustomView(customView);
