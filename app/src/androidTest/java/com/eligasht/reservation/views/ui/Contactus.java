c960224f43f01e652300ab30aa36f277225d341e
Reza.nejati <eli123456>
2018-04-17 16:43:23 +0430

    nejati

--- app/src/androidTest/java/com/eligasht/reservation/views/ui/Contactus.java
+++ app/src/androidTest/java/com/eligasht/reservation/views/ui/Contactus.java
@@ -1,107 +1,27 @@
 package com.eligasht.reservation.views.ui;
 
-import android.os.Bundle;
-import android.support.test.espresso.ViewInteraction;
 
 import com.eligasht.R;
 import com.eligasht.reservation.views.TestConst;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
-import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.is;
 
-/**
- * Created by Ahmad.nemati on 4/17/2018.
- */
-public class Contactus extends BaseTest {
+public class ContactUs extends BaseTest {
 
-    public static Contactus newInstance() {
-        return new Contactus();
+    public static ContactUs newInstance() {
+        ContactUs fragment = new ContactUs();
+        return fragment;
     }
     @Override
     public void runTest() {
         doClick(R.id.btnMenu);
         doClick(R.id.btnContactUs);
-        sleep(2000);
-        ViewInteraction imageView = onView(
-                allOf(withContentDescription("Zoom in"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.RelativeLayout")),
-                                        2),
-                                0),
-                        isDisplayed()));
-        imageView.perform(click());
-
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-        sleep(2000);
-
-        ViewInteraction imageView2 = onView(
-                allOf(withContentDescription("Zoom in"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.RelativeLayout")),
-                                        2),
-                                0),
-                        isDisplayed()));
-        imageView2.perform(click());
-
-        ViewInteraction imageView3 = onView(
-                allOf(withContentDescription("Zoom out"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.RelativeLayout")),
-                                        2),
-                                1),
-                        isDisplayed()));
-        imageView3.perform(click());
-
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-        sleep(2000);
-
-        ViewInteraction imageView4 = onView(
-                allOf(withContentDescription("Zoom out"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.RelativeLayout")),
-                                        2),
-                                1),
-                        isDisplayed()));
-        imageView4.perform(click());
-
-        ViewInteraction imageView5 = onView(
-                allOf(withContentDescription("Zoom out"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.RelativeLayout")),
-                                        2),
-                                1),
-                        isDisplayed()));
-        imageView5.perform(click());
-
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-        sleep(2000);
-
-        ViewInteraction imageView6 = onView(
-                allOf(withContentDescription("Zoom out"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.RelativeLayout")),
-                                        2),
-                                1),
-                        isDisplayed()));
-        imageView6.perform(click());
-        pressBack(TestConst.Contactus_Back);
-
+        sleep(2500);
+        doClick(R.id.btnBack);
+        doClick(R.id.btnMenu);
+       /* doClick(R.id.edit_email_resetPass);
+        doReplace(R.id.edit_email_resetPass, TestConst.Email_Forget);
+        doPressImeActionButton(R.id.edit_email_resetPass);
+        doClick(R.id.btnResetPassword);*/
+        sleep(2500);
     }
 }
