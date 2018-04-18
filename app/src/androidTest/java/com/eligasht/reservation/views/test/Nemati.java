138d82e7e1a151e85efe6df913b97aae70bd976c
Ahmad.nemati <eli12345678>
2018-04-18 13:41:33 +0430

    Merge branch 'master' of http://192.168.115.210:8080/tfs/Eligasht%20Android%20App%20Ver.1/_git/EligashtAndroidApp into nemati
    # Conflicts:
    #	app/src/main/java/com/eligasht/reservation/views/fragments/HotelFlightFragment.java

--- app/src/androidTest/java/com/eligasht/reservation/views/test/Nemati.java
+++ app/src/androidTest/java/com/eligasht/reservation/views/test/Nemati.java
@@ -2,6 +2,7 @@ package com.eligasht.reservation.views.test;
 
 import com.eligasht.reservation.views.ui.AppStartup;
 import com.eligasht.reservation.views.ui.BaseTest;
+import com.eligasht.reservation.views.ui.HotelF;
 
 /**
  * Created by Ahmad.nemati on 4/18/2018.
@@ -10,7 +11,7 @@ public class Nemati extends BaseTest {
     @Override
     public void runTest() {
         AppStartup.newInstance().runTest();
-       // HotelF2.newInstance().runTest();
+        HotelF.newInstance().runTest();
         sleep(100000);
 
     }
