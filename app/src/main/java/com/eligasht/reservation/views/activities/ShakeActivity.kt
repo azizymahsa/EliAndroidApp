71966e7da5b3ae86f7c88607c776a10e9570e9a1
Ahmad.nemati <eli12345678>
2018-05-09 10:54:50 +0430

    add kotlin

--- app/src/main/java/com/eligasht/reservation/views/activities/ShakeActivity.kt
+++ app/src/main/java/com/eligasht/reservation/views/activities/ShakeActivity.kt
@@ -102,7 +102,7 @@ class MainActivity : AppCompatActivity(), ShakeDetector.Listener {
 
         hs!!.setSensitivity(SENSITIVITY_HARD)
         hs!!.start(hardSensor)
-        parallaxLayout!!.setTranslationUpdater(AnimatedTranslationUpdater(1f))
+        parallaxLayout!!.setTranslationUpdater(translationUpdater)
     }
 
     private fun initSound() {
