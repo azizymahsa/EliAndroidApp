1fd25f7a43a32b23da6b478caac313b1290e1ba0
Ahmad.nemati <eli12345678>
2018-05-30 10:30:59 +0430

    flight seat

--- app/src/main/java/com/eligasht/reservation/base/SingletonAnalysis.java
+++ app/src/main/java/com/eligasht/reservation/base/SingletonAnalysis.java
@@ -1,6 +1,10 @@
 package com.eligasht.reservation.base;
+
+import android.os.Bundle;
+
 import com.eligasht.reservation.views.ui.SingletonContext;
 import com.google.firebase.analytics.FirebaseAnalytics;
+
 /**
  * Created by Reza Nejati on 29,May,2018
  */
@@ -13,10 +17,45 @@ public class SingletonAnalysis {
     }
 
     private SingletonAnalysis() {
-        firebaseAnalytics=FirebaseAnalytics.getInstance(SingletonContext.getInstance().getContext());
+        firebaseAnalytics = FirebaseAnalytics.getInstance(SingletonContext.getInstance().getContext());
     }
 
-    public FirebaseAnalytics getFirebaseAnalytics() {
-        return firebaseAnalytics;
+    public void LogSeen(ServiceType serviceType) {
+        Bundle bundle = new Bundle();
+        bundle.putString("Seen", "Seen");
+        sendLog(serviceType, bundle);
     }
+
+    public void logTransfer(ServiceType serviceType, String origin, String dest) {
+        Bundle bundle = new Bundle();
+        bundle.putString("Origin", origin);
+        bundle.putString("Destination", dest);
+        sendLog(serviceType, bundle);
+    }
+
+
+    private void sendLog(ServiceType serviceType, Bundle bundle) {
+        String eventName = null;
+        switch (serviceType) {
+            case FLIGHT:
+                eventName = "FLIGHT";
+                break;
+            case HOTEL:
+                eventName = "HOTEL";
+                break;
+            case HOTELFLIGHT:
+                eventName = "HOTEL_FLIGHT";
+                break;
+            case PACKAGE:
+                eventName = "PACKAGE";
+                break;
+            case INSURANCE:
+                eventName = "INSURANCE";
+                break;
+
+        }
+
+        firebaseAnalytics.logEvent(eventName, bundle);
+    }
+
 }
