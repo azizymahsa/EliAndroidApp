97a55f9bd452bc2b20c397d8c70ed7ff51838e0d
Ahmad.nemati <eli12345678>
2018-05-30 11:53:57 +0430

    flight seat

--- app/src/main/java/com/eligasht/reservation/base/BaseActivity.java
+++ app/src/main/java/com/eligasht/reservation/base/BaseActivity.java
@@ -9,9 +9,13 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AlertDialog;
-import com.adjust.sdk.Adjust;
-import com.adjust.sdk.AdjustEvent;
-import com.github.bluzwong.swipeback.SwipeBackActivityHelper;
+import android.widget.Toast;
+
+import com.eligasht.reservation.models.eventbus.TerminateBus;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
@@ -19,57 +23,83 @@ import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
  * Created by Reza.nejati on 1/2/2018.
  */
 
-public class BaseActivity extends Base {
-  private BroadcastReceiver sendStartTimer;
+public abstract class BaseActivity extends Base {
+    private BroadcastReceiver sendStartTimer;
 
-  @Override
-  protected void onCreate(@Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    notiRecive();
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        notiRecive();
+        try {
+            EventBus.getDefault().register(this);
+        } catch (Exception e) {
 
-  }
+        }
 
-
-  @Override
-  protected void attachBaseContext(Context context) {
-    super.attachBaseContext(CalligraphyContextWrapper.wrap(context));
-  }
-
-  public void notiRecive() {
-    sendStartTimer = new BroadcastReceiver() {
-      @Override
-      public void onReceive(Context context, Intent intent) {
-
-        finish();
-      }
-    };
-    LocalBroadcastManager.getInstance(this).registerReceiver(sendStartTimer,
-        new IntentFilter("sendFinish"));
-  }
-
-  protected ProgressDialog mProgressDialog;
-  protected AlertDialog mAlertDialog;
-
-  protected void needShowProgressDialog() {
-    if (mProgressDialog == null) {
-      mProgressDialog = new ProgressDialog(this);
-      mProgressDialog.setMessage("لطفا کمی صبر کنید ...");
-      mProgressDialog.setIndeterminate(true);
     }
-    mProgressDialog.show();
 
-  }
 
-  protected void needHideProgressDialog() {
-    if (mProgressDialog != null && mProgressDialog.isShowing()) {
-      mProgressDialog.dismiss();
+    @Override
+    protected void attachBaseContext(Context context) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(context));
     }
-  }
 
-  @Override
-  protected void onResume() {
-    super.onResume();
+    public void notiRecive() {
+        sendStartTimer = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+                finish();
+            }
+        };
+        LocalBroadcastManager.getInstance(this).registerReceiver(sendStartTimer,
+                new IntentFilter("sendFinish"));
+    }
+
+    protected ProgressDialog mProgressDialog;
+    protected AlertDialog mAlertDialog;
+
+    protected void needShowProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage("لطفا کمی صبر کنید ...");
+            mProgressDialog.setIndeterminate(true);
+        }
+        mProgressDialog.show();
+
+    }
+
+    protected void needHideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
 
 
-  }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            EventBus.getDefault().unregister(this);
+        } catch (Exception e) {
+
+        }
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void terminate(TerminateBus terminateBus) {
+        try {
+            if (needTerminate()) {
+                Toast.makeText(this, "زمان نشست پایان یافته است", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+
+        } catch (Exception e) {
+
+        }
+    }
+
+
+    public abstract boolean needTerminate();
 }
---
+++ app/src/main/java/com/eligasht/reservation/models/eventbus/TerminateBus.java
@@ -0,0 +1,8 @@
+package com.eligasht.reservation.models.eventbus;
+
+/**
+ * Created by Ahmad.nemati on 5/30/2018.
+ */
+public class TerminateBus {
+
+}
