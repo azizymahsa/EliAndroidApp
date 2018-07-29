e49795e94618ba37891f301b105cc85e15c2c137
Mahsa.azizi <mahsa.azizi@eligasht.com>
2018-07-29 14:24:50 +0430

    Add AGC_Id in passenger Insurance and Package , Test number factor with Ms mohebi

--- app/src/main/java/com/eligasht/reservation/views/activities/pack/PassengerPackageActivity.java
+++ app/src/main/java/com/eligasht/reservation/views/activities/pack/PassengerPackageActivity.java
@@ -846,7 +846,6 @@ public class PassengerPackageActivity extends BaseActivity implements Header.onS
                         ((EditText) findViewById(R.id.txt_NationalCode_m)).setTextColor(Color.parseColor("#4d4d4d"));
                         flagMosafer = flagMosafer + "T";
                     } else {
-                        //((EditText)findViewById(R.id.txtnumber_passport)).setTextColor(Color.parseColor("#ff3300"));
                         flagMosafer = flagMosafer + "F";
                         errorMessagePartner = errorMessagePartner + "\n" + "* " + getString(R.string.The_national_code_is_not_correct);
                     }
